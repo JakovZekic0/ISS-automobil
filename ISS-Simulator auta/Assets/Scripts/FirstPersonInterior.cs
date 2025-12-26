18fa24c63ef55f9f769f6cdc4ad4f272ac9cdbd9
Luka Šepec <ls001615831@fer.hr>
2025-12-26 23:28:37 +0100

    Added first person camera interior
    Steering wheel, spedometer, current speed, rearview mirrors

---
+++ ISS-Simulator auta/Assets/Scripts/FirstPersonInterior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class FirstPersonInterior : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private Image SpeedometerNeedle;
+    [SerializeField] private RawImage SteeringWheel;
+    [SerializeField] private CarController carController;
+    [SerializeField] private TextMeshProUGUI text;
+
+
+    float steeringWheelCurrentAngle;
+    float needleCurrentAngle;
+    float carSpeed;
+    void Start()
+    {
+        canvasGroup.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (canvasGroup.alpha == 0) canvasGroup.alpha = 1;
+            else canvasGroup.alpha = 0;
+        }
+
+        //steering wheel
+        float steerInput = Input.GetAxis("Horizontal"); // -1 .. 1
+        float targetAngle = -steerInput * 90;
+        steeringWheelCurrentAngle = Mathf.Lerp(steeringWheelCurrentAngle, targetAngle, Time.deltaTime * 8f);
+        SteeringWheel.transform.localRotation = Quaternion.Euler(0f, 0f, steeringWheelCurrentAngle);
+
+        //speed info
+        carSpeed = Mathf.Floor(carController.Carspeed());
+        text.text = $"{(int)carSpeed} Kmh";
+
+        //speedometer needle
+        float needleAngle = (carSpeed / 120) * -270f;
+        needleCurrentAngle = Mathf.Lerp(needleCurrentAngle, needleAngle, Time.deltaTime * 8f);
+        SpeedometerNeedle.transform.localRotation = Quaternion.Euler(0f, 0f, needleCurrentAngle);
+
+
+    }
+}
